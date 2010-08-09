262a1e3e50241d0adc551a829c5eb06e0abb4cef
kaaquantum <kaaquantum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-09 04:08:01 +0000

    git-svn-id: https://plugins.svn.wordpress.org/dynamic-blogname/trunk@273334 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dynamic-blogname.php
+++ dynamic-blogname.php
@@ -9,34 +9,34 @@ Author URI: http://andrewkiselev.com
 License: GPL2
 */
 
-//$file = plugins_url('dyn-blogname/titles.txt');
+//$file = plugins_url('dynamic-blogname/titles.txt');
 //$blogtitle = bloginfo('name');
-
-function dyn_blogname_activation(){
+/*
+function dynamic_blogname_activation(){
 	// create titles.txt
-	$file = plugins_url('dyn-blogname/titles.txt');
+	$file = plugins_url('dynamic-blogname/titles.txt');
 	//$content = bloginfo('name');	
 	$content = 'Blogname';		
 
 	file_put_contents($file, $content, FILE_APPEND);
 }
 
-register_activation_hook( __FILE__, 'dyn_blogname_activation' );
-
+register_activation_hook( __FILE__, 'dynamic_blogname_activation' );
+*/
 
 
 // Return a random title read from a special file that contains a list of title to choose from.
 function random_title(){
 	
-	$lines = file(plugins_url('dyn-blogname/titles.txt'), FILE_SKIP_EMPTY_LINES);        
+	$lines = file(plugins_url('dynamic-blogname/titles.txt'), FILE_SKIP_EMPTY_LINES);        
 	$title = $lines[array_rand($lines)];
 	return $title;
 }
 
-add_filter('bloginfo', 'dyn_blogname', 1, 2);
+add_filter('bloginfo', 'dynamic_blogname', 1, 2);
 
 // Filter for wordpress that redefines output of bloginfo('name')
-function dyn_blogname($result='', $show='') {
+function dynamic_blogname($result='', $show='') {
         switch ($show) {
         case 'name':
                 $result = random_title();
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: andrewkiselev.com
 Tags: appearence, blogname, site title, title
 Requires at least: 3.0
 Tested up to: 3.0.1
-Stable tag: 1.0
+Stable tag: 1.0.1
 
 The plugin allows you to change and rotate the title of your blog dynamicaly every reloading.
 
@@ -25,5 +25,8 @@ Just use a button "Add New" in Plugin section of your Wordpress blog's Control p
 == Changelog ==
 
 = 1.0 =
-* The first stable workin' version.
+* The first stable workin' release at the Wordpress.org plugin repository.
+
+= 1.0.1 =
+* Fixed mistake in titles.txt's location code that leads to excessive load.
 
